e45989d98e4650a4c28c02a561b237ca6b91f1a3
Kaleb Tsegaye <kfesaha15@gmail.com>
2025-11-01 15:41:08 -0500

    Add experimental appDir support in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // no output: 'export'
+  experimental: {
+    appDir: true,  // if using the App router
+  },
+  images: {
+    unoptimized: false,  // if you want to use built‑in optimization
+  },
 };
 
 export default nextConfig;
